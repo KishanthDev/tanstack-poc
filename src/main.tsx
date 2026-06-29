5be14bec80916a4f6036244dc3b07c686fadd381
KishanthDev <kishanthkisna@gmail.com>
2026-06-29 12:25:33 +0530

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { ThemeProvider } from "./components/ThemeProvider";
-import "./index.css";
-
+import "./index.css"
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <ThemeProvider
